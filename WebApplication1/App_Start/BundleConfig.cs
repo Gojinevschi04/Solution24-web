de7801ae60846123722f0670a2ed9512f803bf73
Gojinevschi04 <annagojinevschi@gmail.com>
2024-02-11 17:11:18 +0200

    Add admin ui

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -8,23 +8,70 @@ namespace WebApplication1
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            // Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/bootstrap.min.js.map",
+                "~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/bootstrap.bundle.min.js.map"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
+            // jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
+                "~/Scripts/jquery.min.js",
+                "~/Scripts/jquery.min.map",
+                "~/Scripts/jquery.slim.min.map",
+                "~/Scripts/jquery.slim.min.js",
+                "~/Scripts/jquery.dataTables.min.js",
+                "~/Scripts/jquery.easing.min.js",
+                "~/Scripts/jquery.easing.compatibility.min.js",
+                "~/Scripts/jquery.slim.min.js"
+            ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/main/js").Include(
+                "~/Scripts/sb-admin-2.min.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+            // Chart
+            bundles.Add(new ScriptBundle("~/bundles/demo/js").Include(
+                "~/Scripts/demo/chart-area-demo.js",
+                "~/Scripts/demo/chart-bar-demo.js",
+                "~/Scripts/demo/chart-pie-demo.js",
+                "~/Scripts/demo/datatables-demo.js"
+            ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/chart-js/js").Include(
+                "~/Scripts/Chart.min.js",
+                "~/Scripts/Chart.bundle.min.js"
+            ));
+
+            // Font Awesome
+            bundles.Add(new ScriptBundle("~/bundles/font-awesome/js").Include(
+                "~/Scripts/font-awesome/all.min.js",
+                "~/Scripts/font-awesome/brands.min.js",
+                "~/Scripts/font-awesome/conflict-detection.min.js",
+                "~/Scripts/font-awesome/fontawesome.min.js",
+                "~/Scripts/font-awesome/regular.min.js",
+                "~/Scripts/font-awesome/solid.min.js",
+                "~/Scripts/font-awesome/v4-shims.min.js"
+            ));
+
+            // SB Admin 2
+            bundles.Add(new StyleBundle("~/bundles/main/css").Include(
+                    "~/Content/sb-admin-2.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/dataTables.bootstrap4.min.css", new CssRewriteUrlTransform()
+                ));
+
+            // Font Awesome
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                    "~/Content/font-awesome/all.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/font-awesome/brands.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/font-awesome/fontawesome.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/font-awesome/regular.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/font-awesome/solid.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/font-awesome/svg-with-js.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/font-awesome/v4-shims.min.css", new CssRewriteUrlTransform())
+            );
         }
     }
 }
